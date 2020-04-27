cc3eed2ff6700feaca8baf38157619f6085f0d42
HuangJie <huangjie007x@gmail>
2020-04-27 16:54:53 +0800

    huangjie

--- tax-service/src/main/java/com/dkm/voucher/dao/VoucherMapper.java
+++ tax-service/src/main/java/com/dkm/voucher/dao/VoucherMapper.java
@@ -30,26 +30,26 @@ public interface VoucherMapper extends IBaseMapper<Voucher> {
      * 获取超市参与人数
      * @return 人数
      */
-    @Select("select count(distinct user_id) from tb_voucher where type_name = 超市")
+    @Select("select count(distinct user_id) from tb_voucher where type_name = '超市'")
     Integer supermarketMuch();
     /**
      * 曾小厨餐厅
      * @return 人数
      */
-    @Select("select count(distinct user_id) from tb_voucher where type_name = 曾小厨餐厅")
+    @Select("select count(distinct user_id) from tb_voucher where type_name = '曾小厨餐厅'")
     Integer restaurantMuch();
     /**
      * 渔乐圈餐厅
      * @return 人数
      */
-    @Select("select count(distinct user_id) from tb_voucher where type_name = 渔乐圈餐厅")
+    @Select("select count(distinct user_id) from tb_voucher where type_name = '渔乐圈餐厅'")
     Integer buildMuch();
 
     /**
      * 成福记餐厅
      * @return 人数
      */
-    @Select("select count(distinct user_id) from tb_voucher where type_name = 成福记餐厅")
+    @Select("select count(distinct user_id) from tb_voucher where type_name = '成福记餐厅'")
     Integer intoRestaurant();
 
     /**
